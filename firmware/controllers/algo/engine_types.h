d494e3dad65a473e800e3cf50b83ab26c9b11015
romioRN <128554106+romioRN@users.noreply.github.com>
2024-08-08 06:19:20 +0300

    Update engine_types.h

--- firmware/controllers/algo/engine_types.h
+++ firmware/controllers/algo/engine_types.h
@@ -427,13 +427,7 @@ enum class trigger_type_e : uint32_t {
 	//
 	// Another point: once you add a new trigger, run get_trigger_images.bat which would run rusefi_test.exe from unit_tests
 	//
-
-  
-  TT_VVT_NISSAN_HR12_IN =84,
-  
-  TT_VVT_NISSAN_HR12_EX =85,
-
-  TT_UNUSED = 86, // this is used if we want to iterate over all trigger types
+  TT_UNUSED = 84, // this is used if we want to iterate over all trigger types
 
 };
 
