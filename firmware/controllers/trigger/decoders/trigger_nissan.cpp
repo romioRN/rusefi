aa28f63d04b2af8fb242f7df24cb53d7a4386a1b
roma_nout <omio234@yandex.ru>
2024-09-01 03:12:37 +0300

    ввт53

--- firmware/controllers/trigger/decoders/trigger_nissan.cpp
+++ firmware/controllers/trigger/decoders/trigger_nissan.cpp
@@ -246,7 +246,7 @@ void initializeNissanHRvvtIn(TriggerWaveform *s) {
 //	s->setSecondTriggerSynchronizationGap(0.1);
 
 	s->setTriggerSynchronizationGap3(/*gapIndex*/0, 5, 7);
-    s->setTriggerSynchronizationGap3(/*gapIndex*/1, 1, 1.3);
+    s->setTriggerSynchronizationGap3(/*gapIndex*/1, 1.1, 1.3);
     s->setTriggerSynchronizationGap3(/*gapIndex*/2, 0.07, 0.3);
 	
 }
