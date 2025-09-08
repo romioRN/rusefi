c65fc596f10b43701f4862f5aeeca70fc5638e67
RomioRN <omio234@yandex.ru>
2025-09-08 20:51:22 +0300

    еще одно испровление

--- firmware/init/sensor/init_hella.cpp
+++ firmware/init/sensor/init_hella.cpp
@@ -5,43 +5,19 @@
 
 static HellaOilLevelSensor hellaSensor(SensorType::OilLevel);
 
-#if EFI_PROD_CODE
-static Gpio hellaPin = Gpio::Unassigned;
-
-static void hellaExtiCallback(void*, efitick_t nowNt) {
-    hellaSensor.onEdge(nowNt);
-}
-#endif
-
 void initHellaOilLevelSensor(bool isFirstTime) {
+    // HellaOilLevelSensor::init() внутри включает EXTI и вызывает Register()
+    // поэтому здесь достаточно только инициализации с пином
 #if EFI_PROD_CODE
-    if (efiExtiEnablePin("hellaOilLevel", engineConfiguration->hellaOilLevelPin,
-        PAL_EVENT_MODE_BOTH_EDGES, hellaExtiCallback, nullptr) < 0) {
-        return;
-    }
-    hellaPin = engineConfiguration->hellaOilLevelPin;
-
-    // перед регистрацией датчика инициализировать его с пином
-    hellaSensor.init(hellaPin);
+    hellaSensor.init(engineConfiguration->hellaOilLevelPin);
 #endif
-
-    // зарегистрировать сенсор в системе
-    hellaSensor.Register();
 }
 
 void deInitHellaOilLevelSensor() {
-    // де-регистрируем сенсор в системе
+    // Сначала де-регистрируем в системе, затем освобождаем ресурсы пина
     hellaSensor.Unregister();
 
 #if EFI_PROD_CODE
-    if (!isBrainPinValid(hellaPin)) {
-        return;
-    }
-
-    // деинициализировать датчик (удалить локальные ресурсы, если нужно)
     hellaSensor.deInit();
-
-    efiExtiDisablePin(hellaPin);
-    hellaPin = Gpio::Unassigned;
 #endif
 }
