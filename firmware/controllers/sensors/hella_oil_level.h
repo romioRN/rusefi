1e577366398936356d41b9bb140741cd68d85bd9
romioRN <128554106+romioRN@users.noreply.github.com>
2025-09-12 05:35:48 +0300

    Update hella_oil_level.h

--- firmware/controllers/sensors/hella_oil_level.h
+++ firmware/controllers/sensors/hella_oil_level.h
@@ -1,47 +1,15 @@
 #pragma once
 
-#include "stored_value_sensor.h"
-// НЕ НУЖЕН include для Timer - он уже есть в pch.h или где-то еще
+// Простые функции доступа (НЕ НУЖЕН КЛАСС)
+void initHellaOilLevelSensor(bool isFirstTime);
+void deInitHellaOilLevelSensor();
 
-class HellaOilLevelSensor : public StoredValueSensor {
-public:
-    explicit HellaOilLevelSensor(SensorType sensorType = SensorType::HellaOilLevel);
-
-    // Инициализация с регистрацией сенсора
-    void init(brain_pin_e pin);
-    void deInit();
-
-    // Обработчик прерывания 
-    void onEdge(efitick_t nowNt, bool value);
-
-    // Getters для калиброванных и сырых значений
-    float getLevelMm() const { return m_levelMm; }
-    float getTempC() const { return m_tempC; }
-    bool isLevelValid() const { return m_levelValid; }
-    bool isTempValid() const { return m_tempValid; }
-
-    // Сырые значения в микросекундах  
-    int32_t getLevelRawPulseUs() const { return static_cast<int32_t>(m_lastPulseWidthLevelUs); }
-    int32_t getTempRawPulseUs() const { return static_cast<int32_t>(m_lastPulseWidthTempUs); }
-
-private:
-    brain_pin_e m_pin = Gpio::Unassigned;
-    Timer m_pulseTimer;         // КАК В FREQUENCY SENSOR
-    Timer m_betweenPulseTimer;  // КАК В FREQUENCY SENSOR
-
-    enum class NextPulse { None, Temp, Level, Diag };
-    NextPulse m_nextPulse = NextPulse::None;
-
-    float m_lastPulseWidthTempUs = 0.0f;
-    float m_lastPulseWidthLevelUs = 0.0f;
-
-    float m_levelMm = 0.0f;
-    float m_tempC = 0.0f;
-    bool m_levelValid = false;
-    bool m_tempValid = false;
-
-    void setLevel(float level, bool valid);
-    void setTemp(float temp, bool valid);
-};
-
-extern HellaOilLevelSensor hellaSensor;
+#if EFI_HELLA_OIL
+// Диагностические функции
+float getHellaOilLevelMm();
+float getHellaOilTempC();
+bool isHellaOilLevelValid();
+bool isHellaOilTempValid();
+uint32_t getHellaOilLevelRawUs();
+uint32_t getHellaOilTempRawUs();
+#endif
