d18594e0e1184900ce7adf6c078047be5d7275fb
RomioRN <omio234@yandex.ru>
2025-09-13 04:52:11 +0300

    hella-10

--- firmware/controllers/sensors/hella_oil_level.cpp
+++ firmware/controllers/sensors/hella_oil_level.cpp
@@ -41,7 +41,7 @@ static void hellaOilCallback(efitick_t nowNt, bool value) {
 
         efiPrintf("  RISE(dt_ms)=%.3f, FSM before=%d", dt_ms, (int)nextPulse);
 
-        if (dt_ms > 55.0f && dt_ms < 95.0f) {
+        if (dt_ms > 30.0f && dt_ms < 95.0f) {
             nextPulse = NextPulse::Temp;
             efiPrintf("  FSM set to TEMP: nextPulse=%d", (int)nextPulse);
         } else if (dt_ms > 96.0f && dt_ms < 150.0f) {
